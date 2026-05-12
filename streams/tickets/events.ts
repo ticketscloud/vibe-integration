37b49efe07536c2a228db030ebde29867b22b1fd
Kirill Klenov <horneds@gmail.com>
2026-05-12 15:39:11 +0300

    refactor(integration): remove protobuf streams, clean up README, and update event categories 🤘

--- streams/tickets/events.ts
+++ streams/tickets/events.ts
@@ -1,60 +1,65 @@
 // Информация о мероприятии
 export type Event = {
-	// Уникальный идентификатор мероприятия
-	id: string;
+  // Уникальный идентификатор мероприятия
+  id: string;
 
-	// Название мероприятия
-	name: string;
+  // Название мероприятия
+  name: string;
 
-	// Категория мероприятия
-	category:
-		| "ballet"
-		| "business"
-		| "concert"
-		| "develpoment"
-		| "excursion"
-		| "exhibition"
-		| "festival"
-		| "health"
-		| "kids"
-		| "movie"
-		| "museum"
-		| "party"
-		| "show"
-		| "sport"
-		| "test"
-		| "theater"
-		| "other";
+  // Категория мероприятия
+  category:
+  | "ballet"
+  | "business"
+  | "circus"
+  | "concert"
+  | "development"
+  | "excursion"
+  | "exhibition"
+  | "festival"
+  | "games"
+  | "health"
+  | "kids"
+  | "movie"
+  | "museum"
+  | "musical"
+  | "party"
+  | "show"
+  | "sport"
+  | "standup"
+  | "test"
+  | "theater"
+  | "workshop"
+  | "other";
 
-	// Дата и время начала и окончания мероприятия (Unix timestamp)
-	start_at: number;
-	finish_at: number;
+  // Дата и время начала и окончания мероприятия (Unix timestamp)
+  start_at: number;
+  finish_at: number;
 
-	// Идентификатор организатора мероприятия
-	organizer_id: string;
+  // Идентификатор организатора мероприятия
+  organizer_id: string;
 
-	// Опциональные детали мероприятия
-	media?: string; // URL изображения мероприятия
-	timezone?: string; // Часовой пояс мероприятия
-	description?: string; // Описание мероприятия
+  // Опциональные детали мероприятия
+  media?: string; // URL изображения мероприятия
+  timezone?: string; // Часовой пояс мероприятия
+  description?: string; // Описание мероприятия
 
-	// Место проведения мероприятия
-	venue?: {
-		// Название места проведения
-		name: string;
+  // Место проведения мероприятия
+  venue?: {
+    // Название места проведения
+    name: string;
 
-		// Адрес места проведения
-		address?: string;
+    // Адрес места проведения
+    address?: string;
 
-		// Дополнительная информация о месте проведения
-		description?: string;
+    // Дополнительная информация о месте проведения
+    description?: string;
 
-		// Почтовый индекс и город места проведения
-		zipcode?: string;
-		city_name?: string;
+    // Почтовый индекс и город места проведения
+    zipcode?: string;
+    city_name?: string;
 
-		// Координаты места проведения
-		lat?: number;
-		lon?: number;
-	};
+    // Координаты места проведения
+    lat?: number;
+    lon?: number;
+  };
 };
